6ccd92d690f5b7fedb2b8c2b46093b19f765cd69
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-11-01 11:56:24 -0800

    Basic site

---
+++ mysite/polling/urls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from django.urls import path
+from polling.views import list_view, detail_view
+
+urlpatterns = [
+    path('', list_view, name="poll_index"),
+    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+]
