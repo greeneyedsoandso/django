65d38634526a7b8d019fba7e5f3b8af5fa4a6535
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-11-07 15:52:43 -0800

    Category now visible in Post admin, no double interface on the Category admin.

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
