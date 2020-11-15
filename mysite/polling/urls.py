b2aba970c680054d864d89c56dc8fef7d5ceebb2
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-11-14 16:51:59 -0800

    Class-based polls from lesson content

--- mysite/polling/urls.py
+++ mysite/polling/urls.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from django.urls import path
-from polling.views import list_view, detail_view
+from polling.views import PollListView, PollDetailView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    path("", PollListView.as_view(), name="poll_index"),
+    path("polls/<int:pk>/", PollDetailView.as_view(), name="poll_detail"),
 ]
