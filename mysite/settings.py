f471913f1cc2fabcddc17cec5f4c05a030a0cf12
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-11-22 20:00:04 -0800

    Working, but ugly

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
     "polling",
     "blogging",
 ]
@@ -122,3 +127,12 @@ USE_TZ = True
 STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
