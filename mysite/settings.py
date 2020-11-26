be3a0a4ac975cbd42665f574f6cd5dd01822028a
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-11-25 16:09:23 -0800

    Still trying to fix a Heroku error

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 
